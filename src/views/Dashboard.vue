48a535182afb7ec4f0f39ea7ee706fc4564edcfa
Max <s724959099@yahoo.com.tw>
2022-01-28 17:48:56 +0800

    feat: add reset password

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -13,6 +13,59 @@
     <div>
       Last login: {{ user.last_login_time }}
     </div>
+    <a-divider/>
+    <a-form :form="form" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }"
+            @submit="handleSubmit">
+      <a-form-item label="Old Password"
+      >
+        <a-input
+          v-decorator="
+          ['oldPassword', { rules: [
+            { required: true, message: 'Please input your password' },
+            {
+              validator: validatePassword,
+            },
+            ] }]"
+          type="password"
+        />
+      </a-form-item>
+      <a-form-item label="Password"
+                   :validate-status="passwordError() ? 'error' : ''" :help="passwordError() || ''"
+      >
+        <a-input
+          v-decorator="
+          ['password', { rules: [
+            { required: true, message: 'Please input your password' },
+            {
+              validator: validatePassword,
+            },
+            ] }]"
+          type="password"
+        />
+      </a-form-item>
+      <a-form-item label="Password Again"
+                   :validate-status="passwordAgainError() ? 'error' : ''"
+                   :help="passwordAgainError() || ''"
+      >
+        <a-input
+          v-decorator="
+          ['passwordAgain', { rules: [
+            { required: true, message: 'Please input your email!' },
+            {
+              validator: compareToFirstPassword,
+            },
+            ] }]"
+          type="password"
+        />
+      </a-form-item>
+
+      <a-form-item>
+        <a-button type="primary" html-type="submit"
+        >
+          Reset Password
+        </a-button>
+      </a-form-item>
+    </a-form>
   </div>
 </template>
 
@@ -24,12 +77,68 @@ export default {
     return {
       user: {},
       loading: true,
+      form: this.$form.createForm(this, { name: 'resetPassword' }),
     };
   },
   methods: {
     logout() {
       this.$axios.delete('/auth/logout/').then(() => {
         window.location.href = '/';
+      });
+    },
+    validateField(fieldName) {
+      const { getFieldError, isFieldTouched } = this.form;
+      return isFieldTouched(fieldName) && getFieldError(fieldName);
+    },
+    passwordError() {
+      return this.validateField('password');
+    },
+    passwordAgainError() {
+      return this.validateField('passwordAgain');
+    },
+    compareToFirstPassword(rule, value, callback) {
+      const { form } = this;
+      if (value && value !== form.getFieldValue('password')) {
+        callback('Two passwords that you enter is inconsistent!');
+      } else {
+        callback();
+      }
+    },
+    validatePassword(rule, value, callback) {
+      if (!value) {
+        callback();
+        return;
+      }
+      if (!/[a-z]/.test(value)) {
+        callback('Contains at least one lower character');
+      } else if (!/[A-Z]/.test(value)) {
+        callback('Contains at least one upper character');
+      } else if (!/[0-9]/.test(value)) {
+        callback('Contains at least one digit character');
+      } else if (!/[^\w\s]/.test(value)) {
+        callback('Contains at least one special character');
+      } else if (!/.{8,}/.test(value)) {
+        callback('Contains at least 8 characters');
+      } else {
+        callback();
+      }
+    },
+    handleSubmit(e) {
+      e.preventDefault();
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          this.$axios.post('/user/reset-password/', {
+            old_password: values.oldPassword,
+            new_password: values.password,
+          }).then((res) => {
+            console.log(res.data);
+          }).catch((err_) => {
+            this.$notification.open({
+              message: 'Reset password error',
+              description: err_.response.data.detail,
+            });
+          });
+        }
       });
     },
   },
