a780e1cfa2a588fff99164fdb1b9bed4ae16b564
Max <s724959099@yahoo.com.tw>
2022-01-30 23:41:56 +0800

    test: deploy

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,8 @@ export default {
   computed: {},
   mounted() {
     console.log('VUE_APP_API_URL', process.env.VUE_APP_API_URL);
-    console.log('TAG', process.env.VUE_APP_TAG);
+    console.log('VUE_APP_TAG', process.env.VUE_APP_TAG);
+    console.log('VUE_APP_DEMO', process.env.VUE_APP_DEMO);
   },
 };
 </script>
